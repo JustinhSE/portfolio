1c9c63e734c5281d042d8e0a5f4b75d0f3c57ab8
justinhSE <justinh.tech1@gmail.com>
2024-07-27 14:46:02 -0400

    public resume

--- app/page.js
+++ app/page.js
@@ -23,7 +23,7 @@ const Home = () => {
               Welcome to my page! I am excited to see you here, feel free to look around! 
             </p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Link href="https://drive.google.com/file/d/1k1tcgytyBRgK7yU4aUw_YuSUQ8u_kv1n/view?usp=sharing">
+              <Link href="https://drive.google.com/file/d/1o2eQc-ZhS0LpuPlHOBHV0XgUx77cuGBv/view?usp=sharing">
                 <Button
                   variant="outline"
                   size="lg"
