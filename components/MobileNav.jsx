64d66fcf0070dcf10f42484115421c8f5bfc6e04
justinhSE <justinh.tech1@gmail.com>
2024-07-30 12:06:25 -0400

    bug fix

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
+import { Sheet, SheetContent, SheetTrigger, SheetClose } from '@/components/ui/sheet'
 import { usePathname } from 'next/navigation';
 import Link from 'next/link'
 import { CiMenuFries } from 'react-icons/ci'
@@ -26,8 +26,8 @@ const links = [
     name: 'contact',
     path: '/contact'
   },
-
 ]
+
 const MobileNav = () => {
   const pathname = usePathname()
   return (
@@ -47,14 +47,15 @@ const MobileNav = () => {
         <nav className='flex flex-col justify-center items-center gap-8'>
           {links.map((link, index) => {
             return (
-              <Link
-                href={link.path}
-                key={index}
-                className={
-                  `${link.path === pathname && 'text-accent border-b-2 border-accent'} text-xl capitalize hover:text-accent transition-all`
-                }>
-                {link.name}
-              </Link>
+              <SheetClose asChild key={index}>
+                <Link
+                  href={link.path}
+                  className={
+                    `${link.path === pathname && 'text-accent border-b-2 border-accent'} text-xl capitalize hover:text-accent transition-all`
+                  }>
+                  {link.name}
+                </Link>
+              </SheetClose>
             );
           })}
         </nav>
