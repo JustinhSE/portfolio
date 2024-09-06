ea3853fe170f02f8dda0f784c0c24f2c0b17ec5c
Justin H <84724234+JustinhSE@users.noreply.github.com>
2024-09-06 08:28:29 -0400

    Update resume

--- app/page.js
+++ app/page.js
@@ -23,7 +23,7 @@ const Home = () => {
               Welcome to my page! I am excited to see you here, feel free to look around! 
             </p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Link href="https://drive.google.com/file/d/1O9CPdpH2FdjCx64iGT04iuE5uhuRhtYt/view?usp=sharing">
+              <Link href="https://drive.google.com/file/d/1HKjN9Q8LPAtH1ISn3Lu8bPiMyp_UlrXU/view?usp=drivesdk">
                 <Button
                   variant="outline"
                   size="lg"
