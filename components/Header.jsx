387767bc624be2d58f01ab3ae05ca652df770654
Justin H <84724234+JustinhSE@users.noreply.github.com>
2025-05-02 15:54:55 -0400

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ import Nav from "./Nav";
 import MobileNav from "./MobileNav";
 const Header = () => {
     return (
-        <header className="py-8 xl:py-12 text-white bg-pink-50/20">
+        <header className="py-8 text-white bg-pink-50/20">
             <div className="container mx-auto flex justify-between items-center">
                 {/* Logo */}
                 <Link href="/">
